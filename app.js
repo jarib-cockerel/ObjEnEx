ddffe349bc13da028ea037fb41155ab8f7a2e558
Jarib Cockerel <jarib.cockerel@outlook.com>
2024-04-16 07:03:38 -0500

    added comments on ES2015 shorthand code

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ function createInstructor(firstName, lastName) {
 
 */
 
+/*
+function createInstructor(firstName, lastName) --> function name with two arguments
+{
+  return {
+    firstName,//return first argument in parameter
+    lastName,//return last argument in parameter
+  };
+}
+
+
+
+*/
+
 function createInstructor(firstName, lastName) {
   return {
     firstName,
